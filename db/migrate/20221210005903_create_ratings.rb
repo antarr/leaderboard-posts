fc1c66637d969fdf04412a5d44294669ff7e3ffd
antarr <antarr.byrd@gmail.com>
2022-12-09 21:39:32 -0600

    Clean Code

--- db/migrate/20221210005903_create_ratings.rb
+++ db/migrate/20221210005903_create_ratings.rb
@@ -7,6 +7,6 @@ class CreateRatings < ActiveRecord::Migration[7.0]
       t.timestamps
     end
 
-    add_index :ratings, [:post_id,:user_id], unique: true
+    add_index :ratings, [:post_id, :user_id], unique: true
   end
 end
